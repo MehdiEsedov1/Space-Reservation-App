b1ce588ce3bbcfbf6066e0fec4d31369ba6e685a
MehdiEsedov1 <mehdiesedof@gmail.com>
2025-06-04 11:55:32 +0400

    Edit comments

--- src/main/java/org/example/persistence/ReservationFileStorage.java
+++ src/main/java/org/example/persistence/ReservationFileStorage.java
@@ -3,7 +3,6 @@ package org.example.persistence;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.example.entity.Reservation;
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
