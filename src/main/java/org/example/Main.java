1cc6e3f3e40a60e647d2924564940513483b08b5
MehdiEsedov1 <mehdiesedof@gmail.com>
2025-06-08 21:49:56 +0400

    Add ProjectStarter plugin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,24 @@
 package org.example;
 
+import org.example.loader.CustomClassLoader;
+import org.example.plugin.MenuAction;
 import org.example.ui.MainMenuConsole;
+
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting Space Reservation System...");
+        try {
+            CustomClassLoader loader = new CustomClassLoader(Path.of("external_plugins"));
+            Class<?> clazz = loader.loadClass("ProjectStarter");
+            MenuAction action = (MenuAction) clazz.getDeclaredConstructor().newInstance();
+
+            action.run();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         new MainMenuConsole().mainMenu();
     }
 }
---
+++ src/main/java/org/example/plugin/MenuAction.java
@@ -0,0 +1,5 @@
+package org.example.plugin;
+
+public interface MenuAction {
+    void run();
+}
